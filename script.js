4461346d54262538e3ad51be3bc853cdd4b9a57f
AVSecure-max <avsverifysecure@gmail.com>
2026-07-10 16:18:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -394,3 +394,23 @@ window.AVS = {
   showNotification,
   storageHelper: window.storageHelper
 };
+
+/animation/
+const reveals = document.querySelectorAll(
+    ".reveal, .reveal-left, .reveal-right, .reveal-scale"
+);
+
+function revealOnScroll() {
+    const trigger = window.innerHeight * 0.85;
+
+    reveals.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+
+        if (top < trigger) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
