6dc1c756583a90c289c9d8c4976027e01ef8975d
AVSecure-max <avsverifysecure@gmail.com>
2026-07-10 16:26:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('AVS Application Loaded');
   
-  window.addEventListener("load", () => {
-    document.body.style.opacity = "1";
-});
-
+ 
   // ===== SMOOTH SCROLLING FOR ANCHOR LINKS =====
   const links = document.querySelectorAll('a[href^="#"]');
   links.forEach(link => {
@@ -395,22 +392,3 @@ window.AVS = {
   storageHelper: window.storageHelper
 };
 
-/animation/
-const reveals = document.querySelectorAll(
-    ".reveal, .reveal-left, .reveal-right, .reveal-scale"
-);
-
-function revealOnScroll() {
-    const trigger = window.innerHeight * 0.85;
-
-    reveals.forEach(el => {
-        const top = el.getBoundingClientRect().top;
-
-        if (top < trigger) {
-            el.classList.add("active");
-        }
-    });
-}
-
-window.addEventListener("scroll", revealOnScroll);
-window.addEventListener("load", revealOnScroll);
