1e96e9ed680da669132c711e74fce1bbbda45dad
AVSecure-max <avsverifysecure@gmail.com>
2026-08-13 11:17:56 -0400

    Remove Firebase configuration from firebase-config.js
    Removed Firebase configuration object from the file.

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,6 @@
 // Firebase Configuration
 // Your AVS Firebase Project Credentials
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDjdpbtA249NKqXufhPQUh-85u3BBrtZ64",
-  authDomain: "av-31d7b.firebaseapp.com",
-  databaseURL: "https://av-31d7b-default-rtdb.firebaseio.com",
-  projectId: "av-31d7b",
-  storageBucket: "av-31d7b.firebasestorage.app",
-  messagingSenderId: "53251636337",
-  appId: "1:53251636337:web:ef02b5cb814227f7a32fd0",
-  measurementId: "G-GX6Y275Q76"
-};
-
 // Initialize Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
